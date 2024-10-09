3bd4d162bac1b6aa891a977ffefb5a00a98db023
Kenan <kenancaparov@gmail.com>
2024-10-09 13:24:40 +0400

    NewsletterBox

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -14,7 +14,7 @@ const LatestCollection = () => {
 
   return (
     <div className="my-10">
-      <div className="text-center py-8 text-3x1">
+      <div className="text-center py-8 text-3xl">
         <Title text1={"LATEST"} text2={"COLLECTIONS"} />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
           Discover our newest arrivals, curated with the latest trends to
