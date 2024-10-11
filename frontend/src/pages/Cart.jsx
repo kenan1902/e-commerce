7ea6dd2b9c8aa98b449fcfd55a473a1ef5639e1e
Kenan <kenancaparov@gmail.com>
2024-10-11 17:23:09 +0400

    Add to Cart Feature

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Cart = () => {
+  const { products, currency, cartItems } = useContext(ShopContext);
+
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[item]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+  }, [cartItems]);
+
   return <div>Cart</div>;
 };
 
